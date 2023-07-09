82b121b90d1ab4efd03a113c245193b81f0731e7
Wenjing Wang <wwjxza@gmail.com>
2023-07-08 23:43:15 -0600

    comments added to completed todoList with one component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -75,20 +75,30 @@ export class AppComponent {
   editTodo(todo) {
     todo.editing = true;
     this.editingTodoTitle = todo.title;
+
   }
 
+  // 引入中间变量 editingTodoTitle 的目的是，用于确认修改后的title是否合理，即是否不为空
+  // 只有不为空时，才更新title；故需要借助这一中间变量，而不是直接将todo.title 与 input box value two-way binding
   editingTodoTitle = '';
 
   updateEditingTodo(todo) {
     if (this.editingTodoTitle.length === 0) {
       return this.removeTodo(todo);
     }
-
     todo.title = this.editingTodoTitle;
     todo.editing = false;
     this.editingTodoTitle = '';
     this.updateStore();
   }
+
+  // 引入中间变量 editingTodoTitle 的目的是，用于确认修改后的title是否合理，即是否不为空
+  // 只有不为空时，才更新title；故需要借助这一中间变量，而不是直接将todo.title 与 input box value two-way binding
+  // 注释掉的就是没有借助中间变量的情况，同时也要改下html里的双向绑定：from [(ngModel)]="editingTodoTitle" to [(ngModel)]="item.title"
+  // updateEditingTodo(todo) {
+  //   todo.editing = false;
+  //   this.updateStore();
+  // }
 
   stopEditing(todo) {
     todo.editing = false;
